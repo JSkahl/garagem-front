f0a4cd6e6fc92b982b997c874db079dae2631e1f
JSkahl <josekahl333@gmail.com>
2024-07-23 14:20:11 -0300

    Commit inicial

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+      // component: () => import('../views/AboutView.vue')
+  ]
+})
+
+export default router
