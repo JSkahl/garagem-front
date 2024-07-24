91a64afe9c15ff545bad2fd07f544a48b92711df
JSkahl <josekahl333@gmail.com>
2024-07-24 09:29:21 -0300

    feat(header): Iniciando a criação da header

---
+++ src/components/SideBar.vue
@@ -0,0 +1,48 @@
+<script setup>
+</script>
+
+<template>
+    <div class="side-bar">
+        <div class="logo">
+            <img src="/logo.png" alt="Logo">
+            <p>Garagem</p>
+        </div>
+        <div class="models">
+            <p>Acessórios</p>
+            <p>Categorias</p>
+            <p>Cores</p>
+            <p>Marcas</p>
+            <p>Modelos</p>
+            <p>Veículos</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.side-bar {
+    display: flex;
+    flex-direction: column;
+    background-color: aqua;
+    width: 15%;
+    height: 100vh;
+}
+
+.logo {
+    display: flex;
+    align-items: flex-end;
+    font-size: 2em;
+}
+
+img {
+    
+    background-color: aliceblue;
+    width: 20%;
+}
+
+.models {
+    background-color: red;
+    height: 30%;
+    font-size: large;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-      // component: () => import('../views/AboutView.vue')
+    {
+      name: "Home",
+      path: "/",
+      component: () => import('@/App.vue')
+    },
   ]
 })
 
