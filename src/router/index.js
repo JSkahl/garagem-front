5a0e54a9b87cb9fdb1c58aaaf0870e63f16ba7b9
JSkahl <josekahl333@gmail.com>
2024-07-26 05:55:20 -0300

    feat(views): Criação das views restantes

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,41 @@ const router = createRouter({
       name: "acessorios",
       path: "/acessorios",
       component: () => import('@/views/AcessorioView.vue'),
+    },    {
+      name: "categorias",
+      path: "/categorias",
+      component: () => import('@/views/CategoriaView.vue'),
+    },    {
+      name: "cores",
+      path: "/cores",
+      component: () => import('@/views/CorView.vue'),
+    },    
+    {
+      name: "marcas",
+      path: "/marcas",
+      component: () => import('@/views/MarcaView.vue'),
+    },    
+    {
+      name: "modelos",
+      path: "/modelos",
+      component: () => import('@/views/ModeloView.vue'),
+    },    
+    {
+      name: "veiculos",
+      path: "/veiculos",
+      component: () => import('@/views/VeiculoView.vue'),
+    },
+    {
+      name: "login",
+      path: "/login",
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      name: "logout",
+      path: "/logout",
+      component: () => import('@/views/LogoutView.vue'),
     }
+
   ]
 })
 
