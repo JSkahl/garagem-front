873255473cd16874402f30d8449589cc07d67fce
JSkahl <josekahl333@gmail.com>
2024-07-26 09:21:43 -0300

    feat(header): adicionando responsividade a header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { ref } from 'vue'
+import Menu from 'vue-material-design-icons/Menu.vue';
+import { useScreen } from '@/composables/screen.js';
+
+const { browserWidth, deviceWidth, isMobile } = useScreen();
+const menuAberto = ref(false);
 
 </script>
 
@@ -19,7 +25,22 @@
         <div class="login">
             <router-link class="link" to="/login">Login</router-link>
         </div>
+        <Menu v-if="isMobile" class="menu-hamburger" @click="menuAberto = !menuAberto" />
+
     </div>
+        <div v-if="menuAberto" class="menu">
+            <div class="models-mobile">
+            <router-link class="link-mobile" to="/">Home</router-link>
+            <router-link class="link-mobile" to="/acessorios">Acessórios</router-link>
+            <router-link class="link-mobile" to="/categorias">Categorias</router-link>
+            <router-link class="link-mobile" to="/cores">Cores</router-link>
+            <router-link class="link-mobile" to="/modelos">Modelos</router-link>
+            <router-link class="link-mobile" to="/veiculos">Veículos</router-link>
+        </div>
+        <div class="login-mobile">
+            <router-link class="link-mobile" to="/login">Login</router-link>
+        </div>
+        </div>
 </template>
 
 <style scoped>
@@ -66,5 +87,55 @@ img {
 
 .link:hover {
     color: rgb(26, 26, 26);
+}
+
+.menu-hamburguer {
+    display: none;
+}
+
+@media (max-width: 768px) {
+    .models, .login {
+        display: none;
+    }
+
+    .logo > p {
+        display: none;
+    }
+
+    .menu {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        border-radius: 10px;
+        height: 89vh;
+        margin-top: 2%;
+        background-color: rgb(230, 230, 230);
+        box-shadow: black 1px 1px 6px;
+        font-size: 1.5rem;
+    }
+
+    .models-mobile {
+        display: flex;
+        flex-direction: column;
+        margin-top: 3%;
+        margin-left: 3%;
+    }
+    
+    .link-mobile {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    .link-mobile:hover {
+        color: rgb(26, 26, 26);
+    }
+
+    .login-mobile {
+        margin: 0 0 3% 3%
+    }
+
+    img {
+        width: 20vw;
+    }
 }
 </style>
